18ee1157ea98296ee7e832616a258dce3de02e8c
AdiKsOnDev <adilalizade13@gmail.com>
2025-07-29 10:47:03 +0400

    chore: Removed dead code

--- src/components/templates/index.ts
+++
@@ -1,9 +0,0 @@
-// Layout components - page layouts and structure
-export { Layout, Container, Flex, Grid } from './Layout';
-
-export type { 
-  LayoutProps, 
-  ContainerProps, 
-  FlexProps, 
-  GridProps 
-} from './Layout';
